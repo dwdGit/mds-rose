68324d4fa4954ab1a6c90735ad97ac6d9f3e6dc6
javar0ne <giuseppe.iavarone19@gmail.com>
2025-01-05 11:37:08 +0100

    feat!: app skeleton

---
+++ src/main/java/com/dg/mdsrose/App.java
@@ -0,0 +1,7 @@
+package com.dg.mdsrose;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("MDS Rose!");
+    }
+}
---
+++ src/test/java/com/dg/mdsrose/AppTest.java
@@ -0,0 +1,10 @@
+package com.dg.mdsrose;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void testApp() {
+        System.out.println("MDS rose!");
+    }
+}
