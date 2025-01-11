2a9442709d4e245cd3d0da79ada8219863fc2fc1
javar0ne <giuseppe.iavarone19@gmail.com>
2025-01-11 12:09:10 +0100

    feat!: add liquibase

--- src/main/java/com/dg/mdsrose/db/DB.java
+++ src/main/java/com/dg/mdsrose/db/DB.java
@@ -5,9 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
+    private static final String DB_URL = "jdbc:sqlite:src/main/resources/db/mds-rose.db";
+
     private DB() {}
 
     public static Connection createConnection() throws SQLException {
-        return DriverManager.getConnection(System.getenv("MDSROSE_URL"));
+        return DriverManager.getConnection(DB_URL);
     }
 }
---
+++ src/main/resources/db/sql/01_user_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    password VARCHAR(64) NOT NULL
+);
